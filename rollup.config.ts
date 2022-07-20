6eba36efb6b2788bfe6dc9daf2ef7e45d210ec44
stelly <stelly@localhost.com>
2022-07-20 16:14:58 +0200

    feat: config rollup config and ts config

---
+++ rollup.config.ts
@@ -0,0 +1,29 @@
+import typeScriptPlugin2 from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+import dts from "rollup-plugin-dts";
+import deleteDts from "rollup-plugin-delete";
+
+const config = [{
+  input: "src/index.ts",
+  output: [{
+    file: "dist/bundle.js",
+    format: "es",
+    name: "KeyDefinitions"
+  }
+  ],
+  plugins: [typeScriptPlugin2({
+    tsconfig: "./tsconfig.json"
+  }), terser()]
+}, {
+  input: "src/index.ts",
+  output: [{
+    file: "dist/bundle.d.ts",
+    format: "esm"
+  }],
+  plugins: [dts(), deleteDts({
+    hook: "buildEnd",
+    targets: ["dist/src", "dist/types"]
+  })]
+}];
+
+export default config;
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export * as Alpha from "./alpha";
+export * as Functions from "./Functions";
+export * as Generals from "./Generals";
+export * as Specials from "./Specials";
+export * as Digits from "./Digits";
+export * as NumPads from "./numpad";
+
+
