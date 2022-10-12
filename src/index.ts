2cca08d5cf36f866ddf7203807e7f16db027eeb0
stelly <stelly@localhost.com>
2022-10-12 23:10:43 +0200

    feat: add compare function; upgrade package json file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import Layout_CRO from "./layouts/cro";
 import { isCharacter } from "../types/guards";
+import { compare } from "../helpers";
 
-export * from './layouts/us';
-export {
- Layout_CRO,
- isCharacter
-};
+export * from "./layouts/us";
+export { Layout_CRO, isCharacter, compare };
